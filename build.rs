4e9b550fab1ec8cfc9e729f3e8416c97f1dd3e01
ko1N <ko1N1337@gmail.com>
2021-02-02 19:52:55 +0000

    Added proper resources to make ReClass.NET on Windows happy

---
+++ build.rs
@@ -0,0 +1,12 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    // compile with default values from Cargo.toml
+    winres::WindowsResource::new().compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {
+}
